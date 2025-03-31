42fa6b8a711eacb95b10cd7b74b90cb6c747a45b
berto9675 <albrtvd@gmail.com>
2025-03-31 09:44:36 +0200

    add OSTest class

--- src/test/java/dev/berto/computers_shop/models/OSTest.java
+++ src/test/java/dev/berto/computers_shop/models/OSTest.java
@@ -1,5 +1,17 @@
 package dev.berto.computers_shop.models;
 
-public class OSTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class OSTest {
+  @Test
+    @DisplayName("Test del enum del sistema operativo del ordenador")
+    void testGetOS() {
+        assertEquals("Windows 10", OS.WINDOWS_10.getOSName());
+        assertEquals("Windows 11", OS.WINDOWS_11.getOSName());
+        assertEquals("MacOS", OS.MACOS.getOSName());
+        assertEquals("Linux", OS.LINUX.getOSName());
+    }
 }
